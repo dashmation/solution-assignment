87b163fce34e5152f09b0972c24cd94434ce2f30
Subhrajyoti Dash <44187454+dashmation@users.noreply.github.com>
2023-05-26 12:53:49 +0530

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -6,4 +6,4 @@ echo 'ppd'
 echo 'prod'
 read -p "Enter environment: " env
 
-newman run pingsafe-sdet-solution.json --timeout-request 500 --global-var env=$env -r htmlextra --reporter-htmlextra-export "pingsafeAPI.html" --reporter-htmlextra-title "Createcustomer API" --reporter-htmlextra-browserTitle "HR API Report" --reporter-htmlextra-titleSize 1 --reporter-htmlextra-showMarkdownLinks --reporter-htmlextra-showGlobalData
+newman run pingsafe-sdet-solution.json --timeout-request 500 --global-var env=$env -r htmlextra --reporter-htmlextra-export "pingsafeAPI.html" --reporter-htmlextra-title "Createcustomer API" --reporter-htmlextra-browserTitle "PINGSAGE API Report" --reporter-htmlextra-titleSize 1 --reporter-htmlextra-showMarkdownLinks --reporter-htmlextra-showGlobalData
